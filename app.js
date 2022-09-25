d34f0c9df3150df8ac129b1e9a0ed8c8fe45162c
kedargogi <kedargogi420@gmail.com>
2022-09-25 14:12:30 +0530

    group chat commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const app = express()
+const cors =  require('cors')
+
+app.use(cors())
+
+app.use(bodyParser.json());
+
+require('crypto').randomBytes(64).toString('hex')
+
+//Database
+const sequelize = require('./util/database');
+
+// Models
+const User = require('./models/user')
+const Message = require('./models/message')
+const Group = require('./models/group')
+const userGroup = require('./models/user-group')
+
+// Routes
+const userRoute = require('./routes/user')
+const messageRoute = require('./routes/message')
+const groupRoute = require('./routes/group')
+const adminRoute = require('./routes/admin')
+
+app.use(userRoute)
+app.use(messageRoute)
+app.use(groupRoute)
+app.use(adminRoute)
+
+// Association
+
+User.hasMany(Message);
+User.hasMany(userGroup);
+
+Group.hasMany(Message);
+Group.hasMany(userGroup);
+
+userGroup.belongsTo(User);
+userGroup.belongsTo(Group);
+
+Message.belongsTo(User);
+Message.belongsTo(Group);
+
+
+sequelize.sync()
+.then(response=>{
+    console.log("Server Running on 3000!!!")
+    app.listen(3000)
+})
+.catch(err=>{
+    console.log(err)
+})
---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('groupchat', 'root', 'Password@91', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
